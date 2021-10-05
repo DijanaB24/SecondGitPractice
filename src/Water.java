5d333d9eabe34e52a6a05446da4d76f2af886943
dijan <dijanamarkic@yahoo.com>
2021-10-05 11:48:21 -0500

    added Water class

---
+++ src/Water.java
@@ -0,0 +1,9 @@
+
+
+
+public class Water {
+    public static void main(String[] args) {
+        System.out.println("The main focus of Black Box Testing is on the functionality of the system as a whole.");
+        System.out.println("The term ‘Behavioral Testing’ is also used for Black Box Testing.");
+    }
+}
