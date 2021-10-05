a76911bb060f1a01fd920b7cce38b485ef05ed5a
dijan <dijanamarkic@yahoo.com>
2021-10-05 11:50:30 -0500

    added Food class

---
+++ src/Food.java
@@ -0,0 +1,6 @@
+public class Food {
+    public static void main(String[] args) {
+        System.out.println("If we take an example to test our car or bike, we always drive it to make sure that it doesn’t behave in an unusual way. See? ");
+        System.out.println("We have already done Black Box Testing.");
+    }
+}
