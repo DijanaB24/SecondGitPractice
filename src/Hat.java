60fbddb313af4bc309e9d4748225fc1d6407fd2a
dijan <dijanamarkic@yahoo.com>
2021-10-05 11:59:12 -0500

    updated Hat class 1st time

--- src/Hat.java
+++ src/Hat.java
@@ -1,5 +1,7 @@
 public class Hat {
     public static void main(String[] args) {
         System.out.println("Most of us perform Black Box Testing every day! ");
+
+        //learning black box testing
     }
 }
